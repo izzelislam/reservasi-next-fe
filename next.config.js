d85181a3beb9b1549ba50a205e11b6412912faab
fkr <bangfkr002@gmail.com>
2024-12-13 07:06:09 +0700

    as

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+      ignoreDuringBuilds: true,
+  },
   // image domain
   images: {
     domains: ['res.cloudinary.com', 'unsplash.com', 'localhost', 'reservasi.tvw-group.com'],
